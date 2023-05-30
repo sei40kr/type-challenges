30a4204929fbb1ac161b28c2d4989c92d5fe265d
Seong Yong-ju <sei40kr@gmail.com>
2023-05-31 06:16:34 +0900

    solve "checkrepeatedchars"

--- questions/09142-medium-checkrepeatedchars/template.ts
+++ questions/09142-medium-checkrepeatedchars/template.ts
@@ -1,1 +1,8 @@
-type CheckRepeatedChars<T extends string> = any
+type CheckRepeatedChars<
+  T extends string,
+  S = {}
+> = T extends `${infer F}${infer R}`
+  ? F extends keyof S
+  ? true
+  : CheckRepeatedChars<R, S & { [K in F]: true }>
+  : false
