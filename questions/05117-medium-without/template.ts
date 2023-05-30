1b3bcf8055329fd2281a2bf3c6c5370cde78b423
Seong Yong-ju <sei40kr@gmail.com>
2023-05-31 06:49:26 +0900

    solve "without"

--- questions/05117-medium-without/template.ts
+++ questions/05117-medium-without/template.ts
@@ -1,1 +1,10 @@
-type Without<T, U> = any
+type Without<
+  T extends any[],
+  U extends T[number] | T[number][]
+> = U extends unknown[]
+  ? T extends [infer F, ...infer R]
+  ? F extends U[number]
+  ? Without<R, U>
+  : [F, ...Without<R, U>]
+  : []
+  : Without<T, [U]>
