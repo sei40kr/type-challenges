7b7ae0c73643e0a8166b6b6568b2fe05b30ab8ad
Seong Yong-ju <sei40kr@gmail.com>
2023-05-31 05:38:42 +0900

    solve "ispalindrome"

--- questions/04037-hard-ispalindrome/template.ts
+++ questions/04037-hard-ispalindrome/template.ts
@@ -1,1 +1,8 @@
-type IsPalindrome<T> = any
+type IsPalindrome<T extends number | string> =
+  `${T}` extends `${infer F}${infer R}`
+  ? `${T}` extends `${F}${infer M}${F}`
+  ? IsPalindrome<M>
+  : R extends ""
+  ? true
+  : false
+  : true
