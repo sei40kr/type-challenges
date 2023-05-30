ba2c55f2c8f2e786fa40b85ad8add9b65bf0d074
Seong Yong-ju <sei40kr@gmail.com>
2023-05-31 07:22:56 +0900

    solve "c-printf-parser"

--- questions/00147-hard-c-printf-parser/template.ts
+++ questions/00147-hard-c-printf-parser/template.ts
@@ -1,11 +1,18 @@
 type ControlsMap = {
-  c: 'char'
-  s: 'string'
-  d: 'dec'
-  o: 'oct'
-  h: 'hex'
-  f: 'float'
-  p: 'pointer'
+  c: "char"
+  s: "string"
+  d: "dec"
+  o: "oct"
+  h: "hex"
+  f: "float"
+  p: "pointer"
 }
 
-type ParsePrintFormat = any
+type ParsePrintFormat<T extends string> =
+  T extends `${infer F}${infer S}${infer R}`
+  ? F extends "%"
+  ? S extends keyof ControlsMap
+  ? [ControlsMap[S], ...ParsePrintFormat<R>]
+  : ParsePrintFormat<R>
+  : ParsePrintFormat<`${S}${R}`>
+  : []
