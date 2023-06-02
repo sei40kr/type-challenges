945cc9fc83ad38b0edc44f8979e917974fbf30d5
Seong Yong-ju <sei40kr@gmail.com>
2023-06-02 22:47:47 +0900

    solve "anyof"

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,3 @@
-type AnyOf<T extends readonly any[]> = any
+type Falsy = false | 0 | "" | [] | { [key: string]: never } | null | undefined
+
+type AnyOf<T extends readonly any[]> = T[number] extends Falsy ? false : true
